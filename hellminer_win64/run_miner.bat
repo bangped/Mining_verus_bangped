3e1e13bb0831a5bc748ca68cce07a6440fb63238
bangped <75523683+bangped@users.noreply.github.com>
2024-01-05 21:25:44 +0700

    Add files via upload

---
+++ hellminer_win64/run_miner.bat
@@ -0,0 +1,6 @@
+REM For more usage info: hellminer.exe --help
+
+REM Example SSL URL
+REM .\hellminer.exe -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+.\hellminer.exe -c stratum+tcp://ap.luckpool.net:3960 -u REKBSZW1r1AKiBBKJ91dTPG98EMaDLegMU.CPU_i3 -p x --cpu 3
